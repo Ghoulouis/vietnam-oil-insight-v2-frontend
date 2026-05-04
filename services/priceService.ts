8506acce6c4ee69a15ef07a8a705c0686fbcea61
Ghoulouis <maikhanh3241@gmail.com>
2026-05-04 19:23:19 +0700

    fix time platts

--- services/priceService.ts
+++ services/priceService.ts
@@ -2,19 +2,31 @@
 import { PlattsPoint } from "@/components/PriceChartV2";
 import { supabase } from "@/lib/supabaseClient";
 
+function toDateOnly(date: Date) {
+  return date.toISOString().split("T")[0];
+}
+const tomorrow = new Date();
+tomorrow.setDate(tomorrow.getDate() + 1);
+
 export async function getPlattsPrices(startDate: Date, endDate: Date): Promise<PlattsPoint[]> {
+  startDate = new Date(startDate);
+  console.log(
+    "Fetching Platts prices from Supabase for date range:",
+    toDateOnly(startDate),
+    "to",
+    toDateOnly(tomorrow),
+  );
   const { data, error } = await supabase
     .from("price")
     .select("date, platts_singapore_diesel_usd")
-    .gte("date", startDate.toISOString())
-    .lte("date", endDate.toISOString())
+    .gte("date", toDateOnly(startDate))
+    .lte("date", toDateOnly(tomorrow))
     .order("date", { ascending: true });
-
   if (error) throw error;
 
   return data.map((item) => {
     return {
-      timestamp: new Date(item.date).getTime() / 1000 + 12 * 3600,
+      timestamp: new Date(item.date).getTime() / 1000 + 9 * 3600,
       price: item.platts_singapore_diesel_usd,
     };
   });
